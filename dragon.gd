3ad58f38072837831947f98c5197894ed1cb7343
PieroganoReggiano <26669430+PieroganoReggiano@users.noreply.github.com>
2023-06-10 00:40:15 +0200

    added animated sprite to dragon

--- dragon.gd
+++ dragon.gd
@@ -2,14 +2,26 @@ extends RigidBody2D
 
 var move_vector : Vector2 = Vector2.ZERO
 
-@export var default_resistance : float = 4.0
-@export var forward_speed : float = 2400.0
-@export var backward_speed: float = 1200.0
+@export var default_resistance : float = 1200.0
+@export var forward_speed : float = 480000.0
+@export var backward_speed: float = 160000.0
 @export var turn_speed: float = 100.0
 
 func set_vector(vec:Vector2):
 	move_vector = vec
+
+func set_animation_idle():
+	var sprite = $"Sprite"
+	if sprite.animation != "idle":
+		sprite.play("idle")
+	sprite.speed_scale = 1.0
 	
+func set_animation_walk(speed:float):
+	var sprite = $"Sprite"
+	if sprite.animation != "walk":
+		sprite.play("walk")
+	sprite.speed_scale = max(1.0, speed / 100.0)
+
 func _physics_process(delta):
 	var acceleration_force = Vector2.ZERO
 	
@@ -18,6 +30,12 @@ func _physics_process(delta):
 	
 	if move_vector.y > 0.0:
 		acceleration_force += Vector2(0.0, -backward_speed)
+		
+	if move_vector != Vector2.ZERO or linear_velocity.length() > 20.0:
+		var walk_animation_speed = linear_velocity.length()
+		set_animation_walk(walk_animation_speed)
+	else:
+		set_animation_idle()
 		
 	acceleration_force = acceleration_force.rotated(rotation)
 	
