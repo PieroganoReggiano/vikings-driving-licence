dabfe579901e8ca2a41b48a68ef243c3f6ea5b7a
PieroganoReggiano <26669430+PieroganoReggiano@users.noreply.github.com>
2023-06-11 10:21:09 +0200

    added bridge and rune for it (with placeholder image)

---
+++ bridge.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+const DragonClass = preload("res://dragon.gd")
+
+var was_ever_open = false
+
+func start_open():
+	if was_ever_open:
+		return
+	was_ever_open = true
+	$AnimatedSprite2D.play("default")
+	#$"Icon".modulate = Color(0.5, 0.5, 0.5, 1.0)
+	
+func finish_open():
+	$"CollisionShape2D".shape = null
+
+
+func _on_body_entered(body):
+	if body is DragonClass:
+		body.drown()
+
+
+func _on_animated_sprite_2d_frame_changed():
+	if $AnimatedSprite2D.frame == 4:
+		finish_open()
